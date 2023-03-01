ed697dfa5941698e5dfe6d265decd978b4c4e2a6
Chloe Kim <chloe@getdozer.io>
2023-03-01 16:13:29 +0800

    chore: add mac-intel formula for brew install
    `brew install dozer --mac_intel` should trigger the download
    
    Signed-off-by: Chloe Kim <chloe@getdozer.io>

--- Formula/dozer.rb
+++ Formula/dozer.rb
@@ -5,6 +5,12 @@ class Dozer < Formula
   sha256 "13f3f957bfb1efdd565d7b1aef96147cbbf3dfce15ab37b9ce765e9562d0ad6b"
   version "0.1.10"
 
+  mac_intel do
+    url "https://github.com/getdozer/homebrew-dozer/releases/latest/download/dozer-mac-intel.tar.gz"
+    sha256 "a5e1995805bd275c45a78c13e2f26fc805343398e1006e7861c5099a4bcc2c09"
+    version "0.1.10"
+  end
+  
   def install
     bin.install "dozer"
   end

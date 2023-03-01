3c3bf8d45335e71e5213262cd27fefd6c184de56
Chloe Kim <chloe@getdozer.io>
2023-03-01 16:17:20 +0800

    chore: reset
    Signed-off-by: Chloe Kim <chloe@getdozer.io>

--- Formula/dozer.rb
+++ Formula/dozer.rb
@@ -4,16 +4,6 @@ class Dozer < Formula
   url "https://github.com/getdozer/homebrew-dozer/releases/latest/download/dozer-mac.tar.gz"
   sha256 "13f3f957bfb1efdd565d7b1aef96147cbbf3dfce15ab37b9ce765e9562d0ad6b"
   version "0.1.10"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a5e1995805bd275c45a78c13e2f26fc805343398e1006e7861c5099a4bcc2c09"
-  end
-  
-  mac_intel do
-    url "https://github.com/getdozer/homebrew-dozer/releases/latest/download/dozer-mac-intel.tar.gz"
-    sha256 "a5e1995805bd275c45a78c13e2f26fc805343398e1006e7861c5099a4bcc2c09"
-    version "0.1.10"
-  end
   
   def install
     bin.install "dozer"

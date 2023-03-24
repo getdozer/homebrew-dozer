0abe30948116b3395ef4b7171e1a0a7fedf79e0c
Dario Pizzamiglio <1150680+mediuminvader@users.noreply.github.com>
2023-03-24 19:36:31 +0800

    Update dozer.rb
    Signed-off-by: Dario Pizzamiglio <1150680+mediuminvader@users.noreply.github.com>

--- Formula/dozer.rb
+++ Formula/dozer.rb
@@ -1,7 +1,7 @@
 class Dozer < Formula
   desc "Open-source low code platform that lets you build data apps in minutes"
   homepage "https://github.com/getdozer/dozer"
-  version "0.1.12"
+  version "0.1.13"
   
   depends_on "openssl"
   depends_on "protobuf"
@@ -9,11 +9,11 @@ class Dozer < Formula
   on_macos do
     on_arm do
       url "https://github.com/getdozer/dozer/releases/latest/download/dozer-macos-arm64-v#{version}.tar.gz"
-      sha256 "a9fc8844a47d867961acc4d0c7f6c91a95e8a16dc58677d8c1e64b850c7b9ca3"
+      sha256 "1257f7bf783e05bb7d924c58bdcb951d6906f27f946f3a8df955a7e56c2c5ac7"
     end
     on_intel do
       url "https://github.com/getdozer/dozer/releases/latest/download/dozer-macos-amd64-v#{version}.tar.gz"
-      sha256 "eda8a2106fc7734ae60bfdd04ff2e745b5f2ff01b2437d668028ea23027f4305"
+      sha256 "e4d266ac7d966b88b5e0fa357a93398b569514dab1ff46dca9e8b77bca27ee92"
     end
   end
   

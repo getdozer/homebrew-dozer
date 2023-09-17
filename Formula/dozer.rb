d47e87375c3e50d0dd5323602dd17a399ea6d4bb
Dario Pizzamiglio <1150680+mediuminvader@users.noreply.github.com>
2023-09-17 15:27:03 +0800

    Update dozer from 0.1.37 to 0.1.38
    Signed-off-by: Dario Pizzamiglio <1150680+mediuminvader@users.noreply.github.com>

--- Formula/dozer.rb
+++ Formula/dozer.rb
@@ -1,18 +1,18 @@
 class Dozer < Formula
   desc "Open-source low code platform that lets you build data apps in minutes"
   homepage "https://github.com/getdozer/dozer"
-  version "0.1.37"
+  version "0.1.38"
   
   depends_on "protobuf"
   
   on_macos do
     on_arm do
       url "https://github.com/getdozer/dozer/releases/download/v#{version}/dozer-macos-aarch64.tar.gz"
-      sha256 "73ff3e167eaccdb48bf80f0dd08dc2114efb76b1e7d2bb64a189adabbdce4333"
+      sha256 "51c7d6c51589618047af1b14a2a41e3a1984d757da05d7218f5ca48e17f9b262"
     end
     on_intel do
       url "https://github.com/getdozer/dozer/releases/download/v#{version}/dozer-macos-amd64.tar.gz"
-      sha256 "5262647293aad2584db413d1f161174ba2116497547c453e7ebbd56902689d00"
+      sha256 "cebf378d365faa3d9f00532ce111c6532db860535f1c7cdc8e9f6f2bf4baa224"
     end
   end
   
